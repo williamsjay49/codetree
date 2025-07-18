a6bc24ea981b908c8c144bce489e349b05ee931c
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-07-18 13:07:24 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 63ms, Memory: 17MB, Status: Passed - Codetree

--- 250718/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 250718/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -1,16 +1,44 @@
-N = int(input())
-str = input()
-arr = [0] * 91
-# Please write your code here.
-for i in range(1, N):
-    if str[0] == str[i]:
-        temp = str[i::]
-        break
-cnt = 1
-for i in range(len(temp)):
-    if temp[i] != str[i]:
-        break
+# N = int(input())
+# str = input()
+# arr = [0] * 91
+# # Please write your code here.
+# for i in range(1, N):
+#     if str[0] == str[i]:
+#         temp = str[i::]
+#         break
+# cnt = 1
+# for i in range(len(temp)):
+#     if temp[i] != str[i]:
+#         break
     
-    cnt += 1
+#     cnt += 1
 
-print(cnt)
+# print(cnt)
+
+
+N = int(input())
+s = input()
+
+arr = list(s)
+
+# Run from string length 1 to the entire length  
+res = float('inf')
+
+for i in range(1, N + 1):  # Set string length
+    tmp = []
+    check = False
+
+    for j in range(N - i + 1):  # Starting point
+        t = ""
+        for k in range(j, j + i):  # Run for the length
+            t += arr[k]
+        
+        if t not in tmp:
+            tmp.append(t)
+        else:
+            check = True
+    
+    if not check:
+        res = min(res, len(tmp[0]))
+
+print(res)
