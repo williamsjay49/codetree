6dcea985692ead4224d6934aa3ec2e025b382627
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-07-18 16:40:52 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 117ms, Memory: 18MB, Status: Passed - Codetree

--- 250718/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 250718/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -1,44 +1,22 @@
-# N = int(input())
-# str = input()
-# arr = [0] * 91
-# # Please write your code here.
-# for i in range(1, N):
-#     if str[0] == str[i]:
-#         temp = str[i::]
-#         break
-# cnt = 1
-# for i in range(len(temp)):
-#     if temp[i] != str[i]:
-#         break
-    
-#     cnt += 1
-
-# print(cnt)
-
-
 N = int(input())
-s = input()
+str = input()
 
-arr = list(s)
+ans = 1
+for i in range(1, N):
+    twice = False
 
-# Run from string length 1 to the entire length  
-res = float('inf')
+    for j in range(N - i + 1):
+        for k in range(j + 1, N - i + 1):
 
-for i in range(1, N + 1):  # Set string length
-    tmp = []
-    check = False
+            issame = True
+            for l in range(i):
+                if str[j + l] != str[k + l]:
+                    issame = False
 
-    for j in range(N - i + 1):  # Starting point
-        t = ""
-        for k in range(j, j + i):  # Run for the length
-            t += arr[k]
-        
-        if t not in tmp:
-            tmp.append(t)
-        else:
-            check = True
-    
-    if not check:
-        res = min(res, len(tmp[0]))
+            if issame:
+                twice = True    
 
-print(res)
+    if twice:
+        ans += 1
+
+print(ans)
