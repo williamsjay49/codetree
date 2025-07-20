d26b514b58f825787cb0202622ef61cf27baea57
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-07-20 22:35:54 +0900

    Title: 독서실의 거리두기 5, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250720/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 250720/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -1,19 +1,36 @@
 N = int(input())
 seat = input()
 
-diff = 0
-start_idx = 0
+# diff = 0
+# start_idx = 0
 
+# for i in range(N):
+#     if seat[i] == '1':  # Seat is occupied
+#         # Calculate the segment length
+#         diff = max(diff, (i - start_idx) // 2)
+#         # print(i, start_idx)
+#         # Update start index to the next position after the '1'
+#         start_idx = i
+
+# # After loop, check the last segment
+# # print(start_idx)
+# diff = max(diff, N - 1 - start_idx)
+
+# print(diff)
+ans = 0
+start_idx = 0
 for i in range(N):
-    if seat[i] == '1':  # Seat is occupied
-        # Calculate the segment length
-        diff = max(diff, (i - start_idx) // 2)
-        # print(i, start_idx)
-        # Update start index to the next position after the '1'
+
+    if seat[i] == "1":
+
+        diff = i - start_idx
+        ans = max(diff // 2, ans)
+
         start_idx = i
 
-# After loop, check the last segment
 # print(start_idx)
-diff = max(diff, N - 1 - start_idx)
 
-print(diff)
+diff = N - 1 - start_idx
+ans = max(diff, ans)
+
+print(ans)
