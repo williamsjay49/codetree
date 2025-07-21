bf9efc3a5b72edb1214553c727a94a9d30c2bca7
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-07-21 14:01:19 +0900

    Title: 가장 작은 x 찾기, Time: 80ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250721/가장 작은 x 찾기/find-smallest-x.py
+++ 250721/가장 작은 x 찾기/find-smallest-x.py
@@ -1,28 +1,49 @@
-n = int(input())
-ranges = [tuple(map(int, input().split())) for _ in range(n)]
+# n = int(input())
+# ranges = [tuple(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
 
-# a, b > interval
-# x 
 
-for j in range(1, 10000):
+# for j in range(1, 10000):
 
-    cnt = 0
-    cur = j
-    for a, b in ranges:
-        exist = False
-        while True:
-            if a <= cur <= b:
-                # print(cur, a, b, cnt)
-                cur *= 2
-                exist = True
-            else:
-                break
+#     cnt = 0
+#     cur = j
+#     for a, b in ranges:
+#         exist = False
+#         while True:
+#             if a <= cur <= b:
+#                 # print(cur, a, b, cnt)
+#                 cur *= 2
+#                 exist = True
+#             else:
+#                 break
         
-        if exist:
-            cnt += 1
-    if cnt == n: 
-        print(j)
+#         if exist:
+#             cnt += 1
+#     if cnt == n: 
+#         print(j)
 
-        break
+#         break
+
+n=int(input())
+ab=[
+    tuple(map(int,input().split()))
+    for _ in range (n)
+]
+
+def in_range(x):
+    for i in range(n):
+        if ab[i][0] <= 2**(i+1)*x and 2**(i+1)*x <= ab[i][1]:
+            continue
+        else:
+            return False
+    return True
+
+ans=[]
+
+for x in range(1,1000):
+    if in_range(x):
+        ans.append(x)
+
+print(min(ans))
+
