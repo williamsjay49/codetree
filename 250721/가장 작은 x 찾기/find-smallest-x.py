8e3b1d14afc4494aa84ecbb2e9631cb1e118a85a
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-07-21 14:05:24 +0900

    Title: 가장 작은 x 찾기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250721/가장 작은 x 찾기/find-smallest-x.py
+++ 250721/가장 작은 x 찾기/find-smallest-x.py
@@ -47,25 +47,29 @@
 
 # print(min(ans))
 
-n=int(input())
-ab=[
-    tuple(map(int,input().split()))
-    for _ in range (n)
-]
+n = int(input())
+if not 1 <= n <= 10:
+    print("Invalid N")
+    exit()
+
+ab = [tuple(map(int, input().split())) for _ in range(n)]
+for a, b in ab:
+    if not (1 <= a <= b <= 10000):
+        print("Invalid range")
+        exit()
 
 def in_range(x):
     for i in range(n):
-        if ab[i][0] <= 2**(i+1)*x and 2**(i+1)*x <= ab[i][1]:
-            continue
-        else:
+        min_val = ab[i][0] / (2 ** (i + 1))
+        max_val = ab[i][1] / (2 ** (i + 1))
+        if not (min_val <= x <= max_val):
             return False
     return True
 
-ans=[]
-
-for x in range(1,1000):
+ans = []
+for x in range(1, 10001):  # Limit to reasonable range based on constraints
     if in_range(x):
         ans.append(x)
+        break  # Stop at the first valid x to optimize
 
-print(min(ans))
-
+print(min(ans) if ans else -1)
