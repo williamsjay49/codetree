d6967fbaa3952baf74ffa4c19996b2131c7a873a
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-07-23 11:44:38 +0900

    Title: 훌륭한 점프, Time: 53ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250723/훌륭한 점프/great-jump.py
+++ 250723/훌륭한 점프/great-jump.py
@@ -1,27 +1,29 @@
 import sys
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
-
+temp = sorted(arr)
 # Please write your code here.
 def max_res(val):
     coll = []
     for i, elem in enumerate(arr):
-        if elem <= val:
+        if elem >= val or i == 0 or i == n - 1:
             coll.append(i)
     
-
+    # print(coll, val)
     for i in range(1, len(coll)):
         # print(coll, val)
         if coll[i] - coll[i - 1 ] > k:
             return False
-    
+
     return True
 
 ans = sys.maxsize
-for i in range(max(arr), min(arr) + 1, -1):
+for i in range(n):
 
-    if max_res(i):
-        # print(i)
-        ans = min(ans, i)
+    if max_res(arr[i]):
+        temp, arr[i] = arr[i], 0
+
+        ans = min(ans, max(arr))
+        arr[i] = temp
 
 print(ans)
