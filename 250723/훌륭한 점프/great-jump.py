96e81c88a9472a082027191faa2e5fecc866fff7
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-07-23 10:01:40 +0900

    Title: 훌륭한 점프, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250723/훌륭한 점프/great-jump.py
@@ -0,0 +1,26 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+def max_res(val):
+    coll = []
+    for i, elem in enumerate(arr):
+        if elem <= val:
+            coll.append(i)
+    
+
+    for i in range(1, len(coll)):
+        # print(coll, val)
+        if coll[i] - coll[i - 1 ] > k:
+            return False
+    
+    return True
+
+ans = 101
+for i in range(max(arr), min(arr) + 1, -1):
+
+    if max_res(i):
+        # print(i)
+        ans = min(ans, i)
+
+print(ans)
