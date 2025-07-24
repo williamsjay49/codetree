8123e9a57062dfca8994f9c17dc48e9cfd64cf27
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-07-24 23:49:16 +0900

    Title: Social Distancing in the Study Room 4, Time: 76ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 250724/Social Distancing in the Study Room 4/study-cafe-keeping-distance-4.py
+++ 250724/Social Distancing in the Study Room 4/study-cafe-keeping-distance-4.py
@@ -1,61 +1,27 @@
-# N = int(input())
-# seat = list(input())
-
-# # Please write your code here.
-
-# def min_num():
-#     dist = N + 2
-
-#     for i in range(N):
-#         for j in range(i + 1, N):
-#             if seat[i] == '1' and seat[j] == '1':
-#                 dist = min(dist, j - i)
-
-#     return dist
-# ans = 0
-# for i in range(N):
-#     if seat[i] != '0':
-#         continue
-        
-#     seat[i] = '1'
-
-#     for j in range(i + 1, N):
-#         if seat[j] != '0':
-#             continue
-            
-#         seat[j] = '1'
-
-#         ans = max(ans, min_num())
-
-#         seat[j] = '0'
-#         seat[i] = '0'
-
-# print(ans)
 N = int(input())
-seat = list(map(int, input().strip()))
+seat = list(input())
 
-max_g = 0
+# Please write your code here.
 
+def min_num():
+    dist = N
+
+    for i in range(N):
+        for j in range(i + 1, N):
+            if seat[i] == '1' and seat[j] == '1':
+                dist = min(dist, j - i)
+
+    return dist
+
+ans = 0
 for i in range(N):
-    if seat[i] == 1:
-        continue
-    for j in range(N):
-        if seat[j] == 1 or j == i:
-            continue
-        
-        seat[i] = 1
-        seat[j] = 1
-        
-        min_g = N + 2
-        for k in range(N):
-            if seat[k] == 1:
-                for l in range(k + 1, N):
-                    if seat[l] == 1:
-                        min_g = min(min_g, l - k)
-                        break
+    for j in range(i + 1, N):
+        if seat[j] == '0' and seat[i] == '0':            
+            seat[j] = seat[i] = '1'
 
-        max_g = max(max_g, min_g)
-        seat[i] = 0
-        seat[j] = 0
+            ans = max(ans, min_num())
 
-print(max_g)
+            seat[j], seat[i] = '0'
+
+print(ans)
+
