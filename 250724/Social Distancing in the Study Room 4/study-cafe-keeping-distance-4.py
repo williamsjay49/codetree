d3b1dc778e515c523978fc8cf0c6a4ac00d0323e
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-07-24 23:45:28 +0900

    Title: Social Distancing in the Study Room 4, Time: 50ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250724/Social Distancing in the Study Room 4/study-cafe-keeping-distance-4.py
+++ 250724/Social Distancing in the Study Room 4/study-cafe-keeping-distance-4.py
@@ -4,7 +4,7 @@ seat = list(input())
 # Please write your code here.
 
 def min_num():
-    dist = N
+    dist = N + 2
 
     for i in range(N):
         for j in range(i + 1, N):
@@ -31,3 +31,31 @@ for i in range(N):
         seat[i] = '0'
 
 print(ans)
+# N = int(input())
+# seat = list(map(int, input().strip()))
+
+# max_g = 0
+
+# for i in range(N):
+#     if seat[i] == 1:
+#         continue
+#     for j in range(N):
+#         if seat[j] == 1 or j == i:
+#             continue
+        
+#         seat[i] = 1
+#         seat[j] = 1
+        
+#         min_g = N + 2
+#         for k in range(N):
+#             if seat[k] == 1:
+#                 for l in range(k + 1, N):
+#                     if seat[l] == 1:
+#                         min_g = min(min_g, l - k)
+#                         break
+
+#         max_g = max(max_g, min_g)
+#         seat[i] = 0
+#         seat[j] = 0
+
+# print(max_g)
