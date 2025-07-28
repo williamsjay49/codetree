a2f3ab3b02b3d7a3d4f76a120b8f541da9ffeef0
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-07-28 14:33:00 +0900

    Title: 이상한 폭탄 3, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250728/이상한 폭탄 3/strange-bomb-3.py
+++ 250728/이상한 폭탄 3/strange-bomb-3.py
@@ -3,24 +3,21 @@ num = [int(input()) for _ in range(n)]
 #가장 많은 터지는 폭탄, 여러개 경우에는 가장 큰 번호, 아무폭탄 터지지 않는 경우 -> 0
 
 ans = 0
-
-for i in range(0, n - k + 1, k):
-    arr = [0] * 1000001
-    cnt = 0
-    for j in range(i, i + k):
-        arr[num[j]] += 1
-ans = max(arr)
-# print(ans)
 temp = 0
-if arr.count(ans) > 1:
-    for i in range(1000000, -1, -1):
-        if arr[i] == ans:
-
-            temp = i
-            break
-
-else:
-    temp = arr.index(ans)
+for i in range(n - k):
+    cnt = 1
+    for j in range(i + 1, i + k + 1):
+        
+        if num[i] == num[j]:
+            cnt += 1
+        # print(i, cnt)
+        
+    if cnt > ans:
+        ans = cnt
+        temp = num[i]
+    
+    elif cnt == ans and cnt != 0:
+        temp = max(temp, num[i])
 
 
 print(temp)
