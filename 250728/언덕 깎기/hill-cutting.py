cd8d64c93e8182dec95d1fc50716c3dba801e0cc
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-07-28 17:39:59 +0900

    Title: 언덕 깎기, Time: 57ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250728/언덕 깎기/hill-cutting.py
@@ -0,0 +1,25 @@
+# n 입력
+n = int(input())
+# hills 입력
+hills = [
+    int(input())
+    for _ in range(n)
+]
+
+min_cost = 100
+for i in range(1, 101):
+    cost = 0
+    for elem in hills:
+        if i <= elem and elem <= i + 17:
+            continue
+        
+        elif i > elem:
+            cost += (i - elem) * (i - elem)
+        
+        else:
+            cost += (elem - (i + 17)) * (elem - (i + 17))
+
+    
+    min_cost = min(min_cost, cost)
+
+print(min_cost)
