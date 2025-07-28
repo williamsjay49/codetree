a6c1942b8406d484df48008cc530113104584927
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-07-28 15:11:26 +0900

    Title: 이상한 폭탄 3, Time: 53ms, Memory: 24MB, Status: Passed - Codetree

--- 250728/이상한 폭탄 3/strange-bomb-3.py
+++ 250728/이상한 폭탄 3/strange-bomb-3.py
@@ -1,50 +1,80 @@
-# n, k = map(int, input().split())
-# num = [int(input()) for _ in range(n)]
-# #가장 많은 터지는 폭탄, 여러개 경우에는 가장 큰 번호, 아무폭탄 터지지 않는 경우 -> 0
-
-# ans = 0
-# temp = 0
-# for i in range(n - k + 1):
-#     cnt = 1
-#     for j in range(i + 1, i + k + 1):
-        
-#         if num[i] == num[j]:
-#             cnt += 1
-#         # print(i, cnt)
-        
-#     if cnt > ans:
-#         ans = cnt
-#         temp = num[i]
-    
-#     elif cnt == ans and cnt != 0:
-#         temp = max(temp, num[i])
-
-
-# print(temp)
-
-
-from collections import Counter
-
 n, k = map(int, input().split())
 num = [int(input()) for _ in range(n)]
+#가장 많은 터지는 폭탄, 여러개 경우에는 가장 큰 번호, 아무폭탄 터지지 않는 경우 -> 0
 
-ans = 0        # Highest explosion count
-res = 0        # Bomb value with highest count
+MAX_A = 1000001
+bomb = [0] * MAX_A
+explode = [False] * n
+maxval = 1
+maxidx = 0
 
-for i in range(n - k + 1):  # inclusive range for last window
-    window = num[i:i + k + 1]
-    count = Counter(window)
-    max_cnt = max(count.values())
+for i in range(n):
+    for j in range(i + 1, n):
+        if j - i > k:
+            break
+        
+        if num[i] != num[j]:
+            continue
+        
+        if explode[i] == False:
+            bomb[num[i]] += 1;
+            explode[i] = True
+        
+        if explode[j] == False:
+            bomb[num[j]] += 1;
+            explode[j] = True
+
     
-    # Find all bomb numbers that appear max_cnt times
-    candidates = [val for val, c in count.items() if c == max_cnt]
-    max_bomb = max(candidates)
+for j in range(MAX_A):
+    if maxval <= bomb[j]:
+        maxval = bomb[j]
+        maxidx = j
 
-    if max_cnt > ans:
-        ans = max_cnt
-        res = max_bomb
-    elif max_cnt == ans:
-        res = max(res, max_bomb)
+print(maxidx)
 
-# If the highest count is 1 (no explosion), return 0
-print(res if ans > 1 else 0)
+
+# MAX_A = 1000000
+
+# # 변수 선언 및 입력
+# n, k = tuple(map(int, input().split()))
+
+# num = [
+#     int(input())
+#     for _ in range(n)
+# ]
+
+# bomb = [0] * (MAX_A + 1)
+# explode = [False] * n
+
+# maxval = 1
+# maxidx = 0
+
+# # 모든 쌍에 대해서 터질 수 있는 폭탄을 찾고
+# # 터진 폭탄의 개수를 저장합니다.
+# for i in range(n):
+#     for j in range(i + 1, n):
+#         # 거리가 k를 초과하는 경우 넘어갑니다.
+#         if j - i > k:
+#             break
+
+#         # 두 폭탄의 번호가 다를 경우 터지지 않습니다.
+#         if num[i] != num[j]:
+#             continue
+
+#         # 두 폭탄의 번호가 같을 경우 폭탄은 터집니다.
+#         # 해당 폭탄이 이미 터진 폭탄인지 확인하고,
+#         # 아직 터지지 않은 폭탄이라면 터진 폭탄의 개수를 갱신합니다.
+#         if explode[i] == False:
+#             bomb[num[i]] += 1;
+#             explode[i] = True
+
+#         if explode[j] == False:
+#             bomb[num[j]] += 1;
+#             explode[j] = True
+
+# for i in range(MAX_A + 1):
+#     if maxval <= bomb[i]:
+#         maxval = bomb[i]
+#         maxidx = i
+
+# print(maxidx)
