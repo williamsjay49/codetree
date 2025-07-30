e64239ff800a44fae186f8b615f413a1593ba55f
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-07-30 17:41:30 +0900

    Title: 최대 H 점수 2, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

--- 250730/최대 H 점수 2/maximum-h-score-2.py
+++ 250730/최대 H 점수 2/maximum-h-score-2.py
@@ -3,18 +3,19 @@ N, L = map(int, input().split())
 a = list(map(int, input().split()))
 
 # Please write your code here.
+for h in range(100, 0, -1):  # Try H from 100 down to 1
+    count_ge_h = 0  # Count of elements ≥ h
+    close_enough = []
 
-for i in range(200, -1, -1):
-    for j in range(N):
-        a[j] += L
-        cnt = 0
-        for k in range(N):
-            if a[k] >= i:
-                cnt += 1
-        
-        if cnt >= i:
-            print(i)
-            sys.exit()
+    for x in a:
+        if x >= h:
+            count_ge_h += 1
+        elif x + 1 >= h:
+            close_enough.append(x)  # Candidate for boosting
 
-        a[j] -= L
-        
+    # You need at least h elements ≥ h
+    needed = h - count_ge_h
+
+    if needed <= len(close_enough) and needed <= L:
+        print(h)
+        sys.exit()
