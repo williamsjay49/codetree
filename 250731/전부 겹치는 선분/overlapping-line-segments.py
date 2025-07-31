66a3ce6e6fc35bb03bf648b6308021be8ad98a85
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-07-31 15:26:04 +0900

    Title: 전부 겹치는 선분, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250731/전부 겹치는 선분/overlapping-line-segments.py
+++ 250731/전부 겹치는 선분/overlapping-line-segments.py
@@ -4,19 +4,47 @@ arr = [tuple(map(int, input().split())) for _ in range(n) ]
 
 # print(arr)
 ans = 0
-for i in range(1, 101):
-    cnt = 0
-    for x1, x2 in arr:
+
+for i in range(n):
+    x1, x2 = arr[i]
+    found = True
+    for j in range(i + 1, n):
+        if x2 < arr[j][0] or arr[j][1] < x1:
+            found = False
+
+    if found:
+        print("Yes")
+        break
+    else:
+        print("No")
+        break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# for i in range(1, 101):
+#     cnt = 0
+#     for x1, x2 in arr:
        
-        # print(x1, x2)
-        if x1 <= i <= x2:
-            cnt += 1
+#         # print(x1, x2)
+#         if x1 <= i <= x2:
+#             cnt += 1
     
 
-    ans = max(ans, cnt)
+#     ans = max(ans, cnt)
 
 
-if ans == n:
-    print("Yes")
-else:
-    print("No")
+# if ans == n:
+#     print("Yes")
+# else:
+#     print("No")
