67904bf88d2aaa21f4b8743ffcfbffb35e0bbb29
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-07-31 15:05:59 +0900

    Title: 구역 청소, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250731/구역 청소/district-cleaning.py
+++ 250731/구역 청소/district-cleaning.py
@@ -1,14 +1,21 @@
+# a, b = map(int, input().split())
+# c, d = map(int, input().split())
+
+# # Please write your code here.
+# ans = 0
+# if b < c or d < a:
+#     print(ans)
+
+# else:
+#     ans = abs(min(a, c) - max(d, b))
+#     print(ans)
+
+
 a, b = map(int, input().split())
 c, d = map(int, input().split())
 
-# Please write your code here.
-ans = 0
-if b < c or d < a:
-    print(ans)
-
+# Check if the intervals [a, b] and [c, d] overlap
+if (a >= c and a <= d) or (b >= c and b <= d) or (c >= a and c <= b) or (d >= a and d <= b):
+    print(max(a, b, c, d) - min(a, b, c, d))
 else:
-    ans = abs(min(a, c) - max(d, b))
-    print(ans)
-
-
-# print(ans)
+    print((b - a) + (d - c))
