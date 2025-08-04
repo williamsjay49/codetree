8be5a1df1450c08cae9bce2e0fe97e772c654864
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-08-04 17:38:38 +0900

    Title: 코딩톡, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250804/코딩톡/coding-talk.py
+++ 250804/코딩톡/coding-talk.py
@@ -27,40 +27,24 @@
 
 n, m, p = map(int, input().split())
 messages = [tuple(input().split()) for _ in range(m)]
-c = [msg[0] for msg in messages]  # sender characters
-u = [int(msg[1]) for msg in messages]  # unread counts
+c = [msg[0] for msg in messages]
+u = [int(msg[1]) for msg in messages]
 
-# Store unread counts by sender letter
-lst = [0] * 26  # lst[0] means for 'A', lst[25] for 'Z'
-how = [''] * m  # sender of each message
+read = [0] * 26  # Tracks how many times each letter was read
 
-for i in range(m):
-    sender = c[i]
-    unread_count = u[i]
-    lst[ord(sender) - ord('A')] = unread_count
-    how[i] = sender
+p_person = u[p - 1]  # Person number for the p-th message (0-indexed)
 
-# If unread count of message p is 0, print nothing
-target_sender = how[p - 1]
-target_unread_count = lst[ord(target_sender) - ord('A')]
-
-if target_unread_count == 0:
-    print("")
-    exit()
-
-# Track who has read messages (size n, since people are A to ...)
-read = [0] * n  # index 0 → A, 1 → B, etc.
-
-# Count same unread value as message[p] BEFORE message[p]
+# Count messages *before* p where the receiver is the same as p_person
 for i in range(p - 1):
-    if lst[ord(how[i]) - ord('A')] == target_unread_count:
-        read[ord(how[i]) - ord('A')] += 1
+    if u[i] == p_person:
+        read[ord(c[i]) - ord('A')] += 1
 
-# From message[p] onward, mark as read
+# Count messages from p (inclusive) to end
 for i in range(p - 1, m):
-    read[ord(how[i]) - ord('A')] += 1
+    read[ord(c[i]) - ord('A')] += 1
 
-# Output people (A to ...) who did not read any of those
-for i in range(n):
-    if read[i] == 0:
-        print(chr(ord('A') + i), end=' ')
+# Print characters not read by p_person
+if p_person != 0:
+    for i in range(n):
+        if read[i] == 0:
+            print(chr(ord('A') + i), end=' ')
