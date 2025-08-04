e8a49a4485e798057bd6dc2c03c8739f56d80696
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-08-04 15:01:02 +0900

    Title: 코딩톡, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250804/코딩톡/coding-talk.py
@@ -0,0 +1,22 @@
+n, m, p = map(int, input().split())
+messages = [tuple(input().split()) for _ in range(m)]
+c = [msg[0] for msg in messages]
+u = [int(msg[1]) for msg in messages]
+
+names = ['a'] * n
+# Please write your code here.
+for i in range(p - 1, m):
+    if i >= (p - 1) and c[i] not in names:
+        names[i] = c[i]
+
+for i in range(n):
+    target = ord('A') + i
+    not_found = True
+    for j in range(n):
+        if target == ord(names[j]):
+            not_found = False
+            break
+    
+    if not_found:
+        print(chr(target), end=" ")
+
