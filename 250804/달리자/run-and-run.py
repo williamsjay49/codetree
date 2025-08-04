a166bbf8e4c07aa280fca0bdaac6a34219c89af4
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-08-04 13:08:43 +0900

    Title: 달리자, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250804/달리자/run-and-run.py
@@ -0,0 +1,62 @@
+n = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+ans = 0
+for i in range(n):
+    if A[i] > B[i]:
+        num = A[i]  - B[i]
+
+        A[i] -= num
+        A[i + 1] += num
+
+        ans += num
+
+
+print(ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# total_moves = 0
+# i = j = 0  # pointers
+
+# while i < n and j < n:
+#     # Skip houses that already have desired number of people
+#     while i < n and A[i] == B[i]:
+#         i += 1
+#     while j < n and A[j] == B[j]:
+#         j += 1
+    
+#     if i == n or j == n:
+#         break
+
+#     # If A[i] > B[i], we have excess people at i
+#     if A[i] > B[i]:
+#         # If A[j] < B[j], we need people at j
+#         if A[j] < B[j]:
+#             move = min(A[i] - B[i], B[j] - A[j])
+#             A[i] -= move
+#             A[j] += move
+#             total_moves += move * abs(j - i)
+#         else:
+#             j += 1
+#     else:
+#         i += 1
+
+# print(total_moves)
