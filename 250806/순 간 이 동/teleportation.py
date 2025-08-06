620a58ccbd9276ae7488ff520814d5de03584a93
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-08-06 17:11:17 +0900

    Title: 순 간 이 동, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250806/순 간 이 동/teleportation.py
+++ 250806/순 간 이 동/teleportation.py
@@ -1,8 +1,9 @@
+import sys
 a, b, x, y = map(int, input().split())
 
 # Please write your code here.
 
-dist = 2000
+dist = sys.maxsize
 
 dist = min(dist, abs(a - y) + abs(x - b))
 dist = min(dist, abs(a - b) + abs(x - y))
