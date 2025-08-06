fdb84394efed3d4ca65c4e6acf1517a5bf68becf
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-08-06 17:10:47 +0900

    Title: 순 간 이 동, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250806/순 간 이 동/teleportation.py
+++ 250806/순 간 이 동/teleportation.py
@@ -2,7 +2,7 @@ a, b, x, y = map(int, input().split())
 
 # Please write your code here.
 
-dist = 1001
+dist = 2000
 
 dist = min(dist, abs(a - y) + abs(x - b))
 dist = min(dist, abs(a - b) + abs(x - y))
