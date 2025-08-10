276bc7c0bcce4d6ce17ce5f15673746f9b017fcc
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-08-10 15:47:42 +0900

    Title: Social Distancing in the Study Room 3, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250810/Social Distancing in the Study Room 3/study-cafe-keeping-distance-3.py
@@ -0,0 +1,27 @@
+N = int(input())
+seats = input()
+
+# Please write your code here.
+idx = 0
+dist = 0
+start = 0
+for i in range(1, N):
+
+    Found = True
+    if int(seats[i]) == 1:
+        idx = i
+        dist = (idx + 1 - start) // 2
+        # print(dist, idx)
+        for j in range(idx + 1, N):
+            if int(seats[j]) == 1 and (j - idx) < dist:
+                start = i
+                i = idx + 1
+
+                # print(start, i)
+                False
+                break
+        
+            idx = j
+        if Found:
+            print(dist)
+            break
