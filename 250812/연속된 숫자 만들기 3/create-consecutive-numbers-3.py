54d25983dd605a4ee621b4da64e99e0c99e11a91
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-08-12 09:37:26 +0900

    Title: 연속된 숫자 만들기 3, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250812/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 250812/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -4,9 +4,6 @@ a = list(map(int, input().split()))
 
 if a[0] + 1 == a[1] and a[1] + 1 == a[2]:
     print(0)
-elif a[0] + 2 == a[1]:
-    print(1)
-elif a[0] + 3 == a[1]:
-    print(2)
 else:
-    print(3)
+    ans = (a[1] - 1) - a[0]
+    print(ans)
