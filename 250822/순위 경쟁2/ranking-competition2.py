6ef9098c3785db2503a9d5f4dc421f53bba59b1c
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-08-22 00:02:21 +0900

    Title: 순위 경쟁2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250822/순위 경쟁2/ranking-competition2.py
@@ -0,0 +1,45 @@
+n = int(input())
+c = []
+s = []
+for _ in range(n):
+    ci, si = input().split()
+    c.append(ci)
+    s.append(int(si))
+
+
+# (-) B, (+) A 0 - A, B
+# Please write your code here.
+arr = [0, 0]
+cnt = 0
+
+if c[0] == "A":
+    arr[0] = s[0]
+else:
+    arr[1] = s[0]
+
+diff = arr[0] - arr[1]
+
+if arr[0] - arr[1] != 0:
+    cnt = 1
+
+for i in range(1, n):
+    a, b = c[i], s[i]
+
+    if c[i] == "A":
+        arr[0] += s[i]
+    else:
+        arr[1] += s[i]
+    
+    if arr[0] - arr[1] == 0 and diff != 0:
+        cnt += 1
+        diff = 0
+    
+    elif arr[0] - arr[1] < 0 and diff >= 0:
+        cnt += 1
+        diff = arr[0] - arr[1]
+    
+    elif arr[0] - arr[1] > 0 and diff <= 0:
+        cnt += 1
+        diff = arr[0] - arr[1]
+
+print(cnt)
