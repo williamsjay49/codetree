aeb1dcd99d46ca1843a7708866687638e401e289
Johnson~ <52906501+williamsjay49@users.noreply.github.com>
2025-08-22 12:26:58 +0900

    Title: 두 번째로 작은 수의 위치, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250822/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.py
@@ -0,0 +1,29 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+# Please write your code here.
+min_val = min(a)
+ans = -1
+diff = 101
+cnt = 0
+NO_DUPLICATE = True
+
+for i in range(n):
+    if a[i] != min_val:
+
+        if a[i] - min_val <= diff:
+            ans = i + 1
+
+            if diff == a[i] - min_val:
+                NO_DUPLICATE = False
+                break
+            
+            diff = a[i] - min_val
+
+            
+    
+if NO_DUPLICATE:
+    print(ans)
+else:
+    print(-1)
+        
